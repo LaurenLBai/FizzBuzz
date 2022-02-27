8970fdad718944298a17a73080c4a78f65ce73a8
LaurenLBai <llbaichtal@gmail.com>
2022-02-27 12:25:52 -0800

    Finished FizzBuzz Program - uploaded code and readme files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # FizzBuzz
+This a coding practice program where it will print the numbers from 1 to 100.
+For multiples of three, the word 'Fizz' will print instead of the number.
+For multiples of five, the word 'Buzz' will print instead of the number.
+For numbers that are multiples of both three and five, the word 'FizzBuzz'
+will print instead of the number.
+
+For example (partial sketch of the output):
+
+1
+2
+3   Fizz
+4
+5   Buzz
+6
+7
+.
+.
+15  FizzBuzz
+.
+.
+33  Buzz
+.
+.
+100 Buzz 
+
+I created the program one case at time and then put it all together.
--- fb.py
+++ fb.py
@@ -1,5 +1,5 @@
 # Lauren Baichtal
-# 2/24/2022
+# 2/27/2022
 
 # (i+1) will print 1 to 100 inclusive (i) by itself would print 
 # from 0 to 99
@@ -17,13 +17,15 @@ for i in range(1,101):
 # we can do that by using the modulo (%) with 3 = remainder 0
 
 for i in range(1,101):
-    if i% 3 == 0:
+    if i % 3 == 0:
         print (str(i) + ' is a multiple of 3')
+    else:
+        print(str(i))
 
 # now we can write a conditional to differenciate between multiples
 # of 3 and multiples of 5
 
-for i in range(1,100):
+for i in range(1,101):
     if i % 3 == 0:
         print (str(i) + ' is a multiple of 3')
     elif i % 5 == 0:
@@ -34,4 +36,27 @@ for i in range(1,100):
     # next we need to label the cases when the numbers are multiples
     # of both 3 and 5
     
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print (str(i) + ' is a multiple of 3 and 5')
+    else:
+        print(str(i))
+
+# When putting all the cases together, order matters (I learned this by doing 
+# it wrong first) the conditional for both cases needs to be placed first, or 
+# else the other conditionals would trigger first and override the case for both
+
+# now I'll put together all the cases and replace the strings with the words
+# Fizz, Buzz and FizzBuzz
+
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print ('FizzBuzz')
+    elif i % 3 == 0:
+        print ('Fizz')
+    elif i % 5 == 0:
+        print ('Buzz')
+    else:
+        print(str(i))
+
     
